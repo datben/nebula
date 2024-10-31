cd8f7f7976e2fe27f53b90abce73d39cd4956212
Benjamin Dumont <benjamin.dumont@swissborg.com>
2024-10-31 19:17:47 +0100

    macro static

--- nebula/src/macros.rs
+++ nebula/src/macros.rs
@@ -1,11 +1,11 @@
 #[macro_export]
 macro_rules! impl_static_account {
     ($type:ty, $discriminant:expr, $owner:expr) => {
-        impl StaticDiscriminanted for $type {
+        impl $crate::traits::discriminant::StaticDiscriminanted for $type {
             const DISCRIMINANT: &'static [u8] = $discriminant;
         }
 
-        impl StaticOwner for $type {
+        impl $crate::traits::owned_account::StaticOwner for $type {
             const OWNER: Pubkey = $owner;
         }
     };
